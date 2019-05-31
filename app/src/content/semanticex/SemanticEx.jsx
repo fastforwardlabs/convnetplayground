f907f92520fb23eff1bd6a2432eb88b6fdb2e966
Victor Dibia <victor.dibia@gmail.com>
2019-05-31 14:36:08 -0400

    udpate semnsearch UI, add functionality to support models and layers

--- app/src/content/semanticex/SemanticEx.jsx
+++ app/src/content/semanticex/SemanticEx.jsx
@@ -21,28 +21,37 @@ import "./semanticex.css"
 //     hideCloseButton: boolean('Hide close button (hideCloseButton)', false),
 //     onCloseButtonClick: action('onCloseButtonClick'), });
 
+function abbreviateString(value, maxLength){ 
+    if (value.length <= maxLength){
+        return value
+    } else {
+        return value.substring(0,maxLength) + " .."
+    } 
+}
+
 class SemanticEx extends Component {
-
-
-
     constructor(props) {
         super(props);
 
         this.state = {
             selecteddataset: 0,
-            datasetslist : [
+            selectedmodel: 0,
+            selectedlayer:0,
+            datasetsList : [
                 { name: "CIFAR100x", css: "active", iconpath: "", description: "CIFAR1000 is a subset of the CIFAR10 dataset containing 32*32 images across 10 categories."},        
                 { name: "ICONIC3K", css: "",  iconpath: "", description: "ICONIC3K is a custom dataset assembled from curated iconic images sourced from Flickr."},
                 { name: "IMAGENET3K", css: "",  iconpath: "", description: "IMAGENET3K is a subset of the imagenet dataset on which all of the models used for feature extraction is based on." }
+            ],
+            modelsList: [
+                {name:"VGG16", css: "active", layers:["block1_pool","block2_pool","block3_pool","block4_pool","block5_pool"], description: "A 16 layer model"},
+                {name:"ResNet",  css: "",layers:["resndt_conv1","resndt_conv1","block3_pool","block4_pool","block5_pool"], description: "A class of Models with Skip Connections"},
+                {name:"Inception", css: "", layers:["inception_conv1","block2_pool","block3_pool","block4_pool","block5_pool"], description: "A model by the Keras Guys"},
+                {name:"GoogleNet",  css: "",layers:["googlenet_conv2","block2_pool","block3_pool","block4_pool","block5_pool"], description: "A large model with lots of layers"}
             ]
         }
 
 
-        this.semsearchIntro = ` Layers in a trained convolutional neural network (CNN) can be used to extract features from images.
-        The idea of semantic image search explores how we can use these extracted features to compare the similarity of images 
-        and hence "search" for similar images. How do different architectures compare? What layers perform better and when?
-        What similarity metrics work best? This demo helps you investigate these questions! `
-
+        
 
     }
 
@@ -51,13 +60,18 @@ class SemanticEx extends Component {
     }
 
     clickDatasetImage(e) {
-        
-        let selectedDatasetIndex = e.target.getAttribute("indexvalue")
-        let dbList = this.state.datasetslist
-        dbList[selectedDatasetIndex].css = "active"
-        dbList[this.state.selecteddataset].css = ""
 
-        this.setState({selecteddataset: selectedDatasetIndex})
+        this.setState({selecteddataset: e.target.getAttribute("indexvalue")})
+    }
+
+    clickModelImage(e){ 
+        this.setState({selectedmodel: e.target.getAttribute("indexvalue")})     
+    }
+
+    clickLayerImage(e){ 
+
+        this.setState({selectedlayer: e.target.getAttribute("indexvalue")})
+
     }
 
     clickAllDatasetImage() {
@@ -67,11 +81,30 @@ class SemanticEx extends Component {
 
 
     render() {
-        let datasetImageList = this.state.datasetslist.map((dsdata, index) => {
+        let datasetImageList = this.state.datasetsList.map((dsdata, index) => {
             return (
-                <div key={dsdata.name + "fullbox"} className="iblock datasetfullbox clickable ">
-                    <div className="datasettitles"> {dsdata.name}</div>
-                    <img key={dsdata.name} onClick={this.clickDatasetImage.bind(this)} src={require("../../images/0.jpg")} alt="" className={"datasetbox rad2 " + dsdata.css} indexvalue={index}  />
+                <div key={dsdata.name + "fullbox" + index} className="iblock datasetfullbox clickable mb10">
+                    <div className="datasettitles"> {dsdata.name.toUpperCase()}</div>
+                    <img  onClick={this.clickDatasetImage.bind(this)} src={require("../../images/0.jpg")} alt="" className={"datasetbox rad2 " + (this.state.selecteddataset == index? "active": "")} indexvalue={index}  />
+                </div>
+            )
+        });
+
+
+        let modelImageList = this.state.modelsList.map((mdata, index) => {
+            return (
+                <div key={mdata.name + "fullbox" + index} className="iblock datasetfullbox clickable mb10 ">
+                    <div className="datasettitles"> {mdata.name.toUpperCase()}</div>
+                    <img  onClick={this.clickModelImage.bind(this)} src={require("../../images/0.jpg")} alt="" className={"datasetbox rad2 " + (this.state.selectedmodel == index? "active": "")} indexvalue={index}  />
+                </div>
+            )
+        });
+
+        let layerImageList = this.state.modelsList[this.state.selectedmodel].layers.map((ldata, index) => {
+            return (
+                <div key={ldata + "fullbox" + index} className="iblock datasetfullbox clickable mb10 ">
+                    <div className="datasettitles"> {abbreviateString(ldata,11).toLowerCase()}</div>
+                    <img  onClick={this.clickLayerImage.bind(this)} src={require("../../images/0.jpg")} alt="" className={"datasetbox rad2 " + (this.state.selectedlayer == index? "active": "")} indexvalue={index}  />
                 </div>
             )
         });
@@ -85,42 +118,68 @@ class SemanticEx extends Component {
             )
         });
 
+        let semsearchIntro = `Layers in a trained convolutional neural network (CNN) can be used to extract features from images.
+        Semantic image search explores how we can use these extracted features to compare the similarity of images 
+        and hence "search" for similar images.  `
+
+        let convnetLayer = `How do features extracted using different model architectures compare? What layers perform better and when?
+        What similarity metrics work best? This demo helps you investigate these questions!`
+
+        let introHeight= "8.5rem"
         return (
             <div>
                  
-                <div className="pb10 sectiontitle"> Semantic Search</div>
+                <div className="pb10 sectiontitle"> What is Semantic Search?</div>
                 <div className="horrule"></div>
-                <InlineNotification
-                    title={"Semantic Search"}
-                    // subtitle={"How it works."}
-                    kind={"info"} 
-                    subtitle={this.semsearchIntro}
-                    style={{ minWidth:'100%', marginBottom: '.5rem'}}
-                />
-                {/* <div className="lh10 bluehightlight"> 
-                 This demo explores the idea of semantic search using convolutional neural networks.
-                 Convolutional Neural Networks are able to learn heriarchical features (or representations) of images 
-                 when trained on a given task such as image classification. Early layers in the model learn to detect low level
-                 features such as lines and shapes while later layers learn to detect more concepts such as eyes, faces etc depending 
-                 on the data used for training.  
-                 </div> */}
-
-
-                <div className="mt20 pb10 sectiontitle" > Select a dataset </div>
-                <div className="horrule mb10"></div>
-                <div className="datasetselectdiv">
-                    {datasetImageList}
+                <div className="flex mt10">
+                    <div className="flex5 mr10 mynotif lightbluehightlight p20">
+                            <div className="boldtext mb10"> Semantic Search</div>
+                            <div className="lh10">{semsearchIntro}</div>
+                    </div>
+                    <div className="flex5  mynotif lightbluehightlight p20">
+                            <div className="boldtext mb10"> Convnet Models and Layer</div>
+                            <div className="lh10">{convnetLayer}</div>
+                    </div>
+    
                 </div>
-                <div className="datasetdescription mt10 p10 lightbluehightlight">{this.state.datasetslist[this.state.selecteddataset].description}</div>
+                
+                <div className="flex flexwrap">
+                    <div className="flex4 mr10">
+                        <div className="mt20 pb10 sectiontitle" > Select Dataset </div>
+                        <div className="horrule mb10"></div>
+                        <div className="datasetselectdiv">
+                            {datasetImageList}
+                        </div>
+                        <div className=" iblock boldtext  iblock boldtext datasetdescription  p10 lightbluehightlight">{this.state.datasetsList[this.state.selecteddataset].name}</div>
+                    </div>
+                    <div className="flex5 mr10">
+                        <div className="mt20 pb10 sectiontitle" > Select Model </div>
+                        <div className="horrule mb10"></div>
+                        <div className="datasetselectdiv">
+                            {modelImageList}
+                        </div>
+                        <div className=" iblock boldtext datasetdescription  p10 lightbluehightlight">{this.state.modelsList[this.state.selectedmodel].name.toUpperCase()}</div>
+                    </div>
+                    <div className="flex5">
+                        <div className="mt20 pb10 sectiontitle" > Select Layer </div>
+                        <div className="horrule mb10"></div>
+                        <div className="scrollwindow  ">
+                             <div className="windowcontent"> {layerImageList} </div>
+                        </div>
+                        <div className=" iblock boldtext datasetdescription  p10 lightbluehightlight"> {this.state.modelsList[this.state.selectedmodel].layers[this.state.selectedlayer].toUpperCase()}</div>
+                    </div>
 
+                </div>
+                
+                
                 <div className="mt20 mb10 sectiontitle"> Perform a Similarity Search</div>
                 <div className="horrule mb10"></div>
                 <div className="flex">
                    <div className="iblock  flex1">
-                    <img src={require("../../images/0.jpg")} className="mainsimilarityimage mr10 iblock" alt=""/>
-                    <div> Selected Image </div>
+                    <img src={require("../../images/0.jpg")} className="mainsimilarityimage rad4 mr10 iblock" alt=""/>
+                    <div className="boldtext"> Selected Image </div>
                    </div>
-                    <div className="iblock flex9">{allDatasetImageList}</div>
+                    <div className="iblock flex10">{allDatasetImageList}</div>
                 </div>
             </div>
         );
