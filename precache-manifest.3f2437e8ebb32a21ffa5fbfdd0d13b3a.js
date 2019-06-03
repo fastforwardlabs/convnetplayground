cf140eabc2c4c60112b1e00db54a44996ab2c6c7
Victor Dibia <victor.dibia@gmail.com>
2019-06-03 11:14:44 -0400

    Updates

---
+++ precache-manifest.3f2437e8ebb32a21ffa5fbfdd0d13b3a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da0654355fe0c40fb2f7a180833d7718",
+    "url": "/fastforwardlabs/convnetplayground/index.html"
+  },
+  {
+    "revision": "e5a848d56d3939a6db0b",
+    "url": "/fastforwardlabs/convnetplayground/static/css/main.3c0899b0.chunk.css"
+  },
+  {
+    "revision": "c0d33efff3f55717dab9",
+    "url": "/fastforwardlabs/convnetplayground/static/js/2.93dc26b9.chunk.js"
+  },
+  {
+    "revision": "e5a848d56d3939a6db0b",
+    "url": "/fastforwardlabs/convnetplayground/static/js/main.d19ce145.chunk.js"
+  },
+  {
+    "revision": "71250c66b62a269ef94c",
+    "url": "/fastforwardlabs/convnetplayground/static/js/runtime~main.58145e54.js"
+  },
+  {
+    "revision": "9952a8d0250a709f21c420da20e3acf2",
+    "url": "/fastforwardlabs/convnetplayground/static/media/layer.9952a8d0.png"
+  },
+  {
+    "revision": "20a139c66464f8c44f9c19ef763d71bb",
+    "url": "/fastforwardlabs/convnetplayground/static/media/model.20a139c6.png"
+  }
+]);
