935bdbb0d7e7cc609e0d396c56daa78e558aff8e
Victor Dibia <victor.dibia@gmail.com>
2019-06-11 09:15:41 -0400

    add support for 8 models in similarity generation

--- utils/model_utils.py
+++ utils/model_utils.py
@@ -22,25 +22,34 @@ import numpy as np
 
 
 def get_model(model_name="vgg16"):
+    tf.keras.backend.clear_session()
     if (model_name == "vgg16"):
         from tensorflow.keras.applications.vgg16 import preprocess_input
         model = VGG16(weights='imagenet', include_top=False)
         return (model, preprocess_input)
+    elif (model_name == "vgg19"):
+        return (tf.keras.applications.vgg19.VGG19(weights='imagenet', include_top=False), tf.keras.applications.vgg16.preprocess_input)
     elif (model_name == "resnet50"):
-        from tensorflow.keras.applications.resnet50 import ResNet50
-        from tensorflow.keras.applications.resnet50 import preprocess_input
-        model = ResNet50(weights="imagenet", include_top=False)
-        return (model, preprocess_input)
-
+        return (tf.keras.applications.resnet50.ResNet50(weights="imagenet", include_top=False), tf.keras.applications.resnet50.preprocess_input)
+    elif (model_name == "mobilenet"):
+        return (tf.keras.applications.mobilenet.MobileNet(weights="imagenet", include_top=False), tf.keras.applications.mobilenet.preprocess_input)
+    elif (model_name == "xception"):
+        return (tf.keras.applications.xception.Xception(weights="imagenet", include_top=False), tf.keras.applications.xception.preprocess_input)
+    elif (model_name == "densenet121"):
+        return (tf.keras.applications.densenet.DenseNet121(weights="imagenet", include_top=False), tf.keras.applications.densenet.preprocess_input)
+    elif (model_name == "inceptionv3"):
+        return (tf.keras.applications.inception_v3.InceptionV3(weights="imagenet", include_top=False), tf.keras.applications.inception_v3.preprocess_input)
+    
 
 def get_supported_models():
-    model_architectures = [{"name": "vgg16"},
+    model_architectures = [
+                            {"name": "vgg16"},
+                            {"name": "vgg19"},
                             {"name": "resnet50"},
-                            # {"name": "resnet152"},
-                            # {"name": "mobilenet"},
-                            # {"name": "xception"},
-                            # {"name": "inceptionv3"},
-                            # {"name": "densenet121"},
+                            {"name": "mobilenet"},
+                            {"name": "xception"},
+                            {"name": "densenet121"},
+                            {"name": "inceptionv3"},
                             ]
     return  model_architectures
 
@@ -58,10 +67,20 @@ def get_all_layer_details(model, layer_list):
 def get_all_model_details():
     model_architectures = get_supported_models()
     model_details = []
+    
+    layer_details = f_utils.load_json_file("app/src/assets/models/layer_details.json")
     for model_detail in model_architectures:
-        model, preprocess = get_model(model_name=model_detail["name"])
+        layer_array = []
+        model_layers_dict = layer_details[model_detail["name"]]
         layer_names = get_model_layer_names(model_detail["name"])
-        model_details.append({"name": model_detail["name"], "layers":get_all_layer_details(model, layer_names)})
+        for layer in layer_names:
+            if layer in model_layers_dict:
+                layer_array.append(model_layers_dict[layer])
+        layer_array = sorted(layer_array, key  = lambda i: i["layer_index"])
+
+        # model, preprocess = get_model(model_name=model_detail["name"])
+        # layer_names = get_model_layer_names(model_detail["name"])
+        model_details.append({"name": model_detail["name"], "layers":layer_array })
 
     return model_details
 
@@ -75,7 +94,7 @@ def get_model_layer_names (model_name):
     layer_list = []
     #   for vgg layers only select pool layers
     if (model_name == "vgg16"):
-        layer_list = ["block1_conv1", "block1_conv2",  "block3_conv1", "block3_conv3",  "block4_conv1", "block4_conv4",  "block5_conv1","block5_conv3"]
+        layer_list = ["block1_conv1", "block1_conv2",  "block3_conv1", "block3_conv3",  "block4_conv1", "block4_conv3",  "block5_conv1","block5_conv3"]
     elif (model_name == "vgg19"):
         layer_list = ["block1_conv1", "block1_conv2",  "block3_conv1", "block3_conv4",  "block4_conv1", "block4_conv4",  "block5_conv1","block5_conv4"]
     elif (model_name == "resnet50"):
@@ -84,10 +103,10 @@ def get_model_layer_names (model_name):
         layer_list = ["conv1", "conv_pw_2",  "conv_dw_3", "conv_dw_4",  "conv_dw_6", "conv_dw_9", "conv_pw_12",  "conv_pw_13"]
     elif (model_name == "xception"):
         layer_list = ["block1_conv1", "block1_conv2",  "conv2d_1", "block3_sepconv1",  "conv2d_3", "block7_sepconv2", "block12_sepconv3",  "block14_sepconv2"]
-    elif (model_name == "densenet"):
-        layer_list = ["conv1_conv", "conv2_block1_1_conv",  "conv2_block6_2_conv", "conv3_block5_1_conv",  "conv4_block23_2_conv", "conv5_block13_2_conv", "conv5_block16_1_conv",  "conv5_block16_2_conv"]
+    elif (model_name == "densenet121"):
+        layer_list = ["pool1", "conv2_block1_1_conv",  "conv2_block6_2_conv", "conv3_block5_1_conv",  "conv4_block23_2_conv", "conv5_block13_2_conv", "conv5_block16_1_conv",  "conv5_block16_2_conv"]
     elif (model_name == "inceptionv3"):
-        layer_list = ["conv2d_1", "conv2d_3",  "conv2d_23", "conv2d_35",  "conv2d_54", "conv2d_75", "conv2d_86",  "mixed10"]
+        layer_list = ["conv2d_1", "conv2d_3",  "conv2d_23", "mixed6",  "conv2d_54", "conv2d_75", "conv2d_86",  "mixed10"]
         
     return layer_list
 
